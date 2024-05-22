38ac0d1c003ebc87cfdb3b879608f61111f16937
Francesco Orifici <francescoorifici6@gmail.com>
2024-05-22 10:02:24 +0200

    primo commit develop

--- src/interceptor/axiosInterceptor.js
+++ src/interceptor/axiosInterceptor.js
@@ -1,20 +1,31 @@
 import axios from 'axios';
 
 
+
+
 export function setAuthorizationToken(token) {
 
-
-    axios.interceptors.request.use(
-      (config) => {
-        if (token) {
-          config.headers.Authorization = `${token}`;
-        } else {
-          delete config.headers.Authorization;
-        }
-        return config;
-      },
-      (error) => {
-        return Promise.reject(error);
+  axios.interceptors.request.use(
+    (config) => {
+      console.log('intercept', config);
+      if (token) {
+        config.headers.Authorization = `${token}`;
+      } else {
+        delete config.headers.Authorization;
       }
-    );
-  }
+      return config;
+    },
+    (error) => {
+      if (error.response && error.response.status === 401) {
+        localStorage.removeItem('current_token');
+        window.location = '/login';
+        // navigate('/login');
+      }
+      return Promise.reject(error);
+    }
+  );
+
+
+
+
+}
